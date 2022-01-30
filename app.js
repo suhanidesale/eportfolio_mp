fb5c3029fdcb7314dcd57880fe5063e611bd9b5a
suhanidesale <deslesuhani02@gmail.com>
2022-01-31 02:07:30 +0530

    JS practise 1

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let count = 0
+
+console.log(count)
+
+var myage = 18
+console.log(myage)
+document.getElementById("count-el").innerText = count
+
+var myage = 18
+
+var humandogratio = 7
+
+let myDogAge = myage*humandogratio
+console.log(myDogAge)
