5168e1558bc92b3a86e97b64fff37266ebb2dd81
suhanidesale <deslesuhani02@gmail.com>
2022-02-02 01:49:35 +0530

    button

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
-let count = 0
+// let count = 0
 
-console.log(count)
+// console.log(count)
 
-var myage = 18
-console.log(myage)
-document.getElementById("count-el").innerText = count
+// var myage = 18
+// console.log(myage)
+// document.getElementById("count-el ").innerText = count
 
-var myage = 18
+// var myage = 18
 
-var humandogratio = 7
+// var humandogratio = 7
 
-let myDogAge = myage*humandogratio
-console.log(myDogAge)
+// let myDogAge = myage*humandogratio
+// console.log(myDogAge)
+
+function increment() {
+    console.log("Button was clicked")
+}
+
+//incrementbtn.addEventListener("click", clickHandler);
