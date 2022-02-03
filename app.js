47bd7be05fb8b677bcd300a4468efd2600c13101
suhanidesale <deslesuhani02@gmail.com>
2022-02-03 23:30:54 +0530

    js 2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // let count = 0
 
 // console.log(count)
@@ -14,8 +13,29 @@
 // let myDogAge = myage*humandogratio
 // console.log(myDogAge)
 
+let countEl = document.getElementById("count-el");
+console.log(countEl)
+let lap1 = 0;
 function increment() {
+    
     console.log("Button was clicked")
+    lap1 = lap1+1;
+    countEl.innerText = lap1;
+    // console.log(lap1++)
 }
 
-//incrementbtn.addEventListener("click", clickHandler);
+function save() {
+    console.log(lap1)
+}
+
+let lap2 = 33;
+let lap3 = 36;
+
+
+function countdown()
+{
+    let final_lap = lap1 + lap2 + lap3;
+    console.log(final_lap)
+};
+
+// countEl.addEventListener("click", increment);
